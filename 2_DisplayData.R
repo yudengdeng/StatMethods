7cdb80824bb5a2eaeb0abce9d8513fb712daa7a5
DD <yudengdeng@gmail.com>
2022-09-06 21:04:47 -0500

    Update 2_DisplayData.R

--- 2_DisplayData.R
+++ 2_DisplayData.R
@@ -1,5 +1,5 @@
 #First download the data into a local folder
-dta_tmp <- read.csv(file = "/Users/YuDengdeng/Dropbox/Classes/Data/ex01-026sevengr.csv")
+dta_tmp <- read.csv(file = "/Users/YuDengdeng/Dropbox/Classes/STAT 5305 2022A/Data/ex01-026sevengr.csv")
 
 #------------------- describe categorical variable ---------------------#
 tbl_tmp <- table(dta_tmp$Gender)
@@ -47,3 +47,54 @@ pnorm(100, mean = mean(dta_tmp$IQ), sd = sd(dta_tmp$IQ), TRUE) #percentage of IQ
 qnorm(0.20, mean = mean(dta_tmp$IQ), sd = sd(dta_tmp$IQ)) #Q1 IQ
 qnorm(0.50, mean = mean(dta_tmp$IQ), sd = sd(dta_tmp$IQ)) #Q2 IQ
 qnorm(0.75, mean = mean(dta_tmp$IQ), sd = sd(dta_tmp$IQ)) #Q3 IQ
+
+
+#First download the data into a local folder
+dta_tmp <- read.csv(file = "/Users/YuDengdeng/Dropbox/Classes/STAT 5305 2022A/Data/ex02-008berries.csv")
+
+#------------------- make a scatter plot of   Antho2 vs Antho1---------------------#
+# just a scatter plot
+plot(dta_tmp$Antho1, dta_tmp$Antho2, main="asdf",
+     xlab="asdf", ylab="asdf", pch=19)
+# scatter plot with smoothed curve and straight line
+scatter.smooth(dta_tmp$Antho1, dta_tmp$Antho2, lpars = list(col = "blue", lwd = 3, lty = 3), 
+               xlab="asdf", ylab="asdf",main="asdf")
+abline(lm(dta_tmp$Antho2~dta_tmp$Antho1), col='red',lwd=3)
+legend('topright', c('Linear','Smoothing'), lty=c(1,2), lwd=c(3,3), col=c('red','blue'))
+
+#The scatterplot( ) function in the car package offers many enhanced features, 
+#including fit lines, marginal box plots, conditioning on a factor, 
+#and interactive point identification. Each of these features is optional.
+install.packages("car") #isntall the package if you do not have one
+library(car) #load the "car" packag
+scatterplot(Antho2 ~ Antho1, data=dta_tmp,
+            xlab="asdf", ylab="asdf",
+            main="asdf")
+n = dim(dta_tmp)[1] #sample size
+#scatter plot with different legends for different cateogires
+#dashed curve is smoother, solid line is fitted line
+dta_tmp$category = sample(c(0,1), replace=TRUE, size=n)
+scatterplot(Antho2 ~ Antho1|category, data=dta_tmp,
+            xlab="asdf", ylab="asdf",
+            main="asdf")
+
+
+#------------------- obtain the correlation coefficient between  Antho2 and Antho1---------------------#
+#calculating using definition
+1/(n-1)*sum((dta_tmp$Antho1 - mean(dta_tmp$Antho1))/sd(dta_tmp$Antho1) * (dta_tmp$Antho2 - mean(dta_tmp$Antho2))/sd(dta_tmp$Antho2))
+#calculating using function
+cor(dta_tmp$Antho1, dta_tmp$Antho2)
+
+
+#------------------- obtain two-way table / contingency table ---------------------##
+#load new data
+dta_tmp <- read.csv(file = "/Users/YuDengdeng/Dropbox/Classes/STAT 5305 2022A/Data/ex02-100titanic.csv")
+# create contingency table r
+head(dta_tmp) #examine the first several rows
+names(dta_tmp) #obtain the column name for each variable
+with(dta_tmp, table(Survived, Pclass))
+#Alternatively, you can get the same result using the "xtabs" function.
+# how to make a contingency table
+xtabs(~Survived + Pclass, data= dta_tmp) 
+
+
